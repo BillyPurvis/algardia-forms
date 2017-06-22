24e040d7ee3cdeb7c108169b248369d7d86de650
Billy Purvis <purvisbilly@outlook.com>
2017-06-23 00:13:16 +0100

    Increase welcome message width

--- resources/assets/js/components/admin/app-user-dashboard.vue
+++ resources/assets/js/components/admin/app-user-dashboard.vue
@@ -1,8 +1,13 @@
 <template>
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-
+            <div class="col-sm-12 user-panel">
+                <div class="col-sm-8">
+                    <h1>Welcome, {{ welcomeResponse }}!</h1>
+                </div>
+                <div class="col-sm-4" v-if="userLoggedIn">
+                    <a class="pull-right" href="/api/logout">Logout</a>
+                </div>
             </div>
         </div>
     </div>
@@ -12,23 +17,43 @@
     import formAnimations from '../admin/mixins/hello.js';
 
     export default {
-        mounted() {
-            console.log('Component mounted.')
-        },
         mixins: [ formAnimations ],
         props: [
             'logged_user',
-            'csrf_token'
         ],
-        methods: {
-            userIsLogged: function () {
-                if(this.is_logged_in !== '') {
-                    console.log('blue')
-                }
-            }
+        mounted() {
+
         },
-        mounted: function () {
-            this.userIsLogged();
-        }
     }
 </script>
+
+<style lang="scss" scoped>
+    @import "../../../sass/variables";
+    h1 {
+        color: #fff;
+        margin: 0;
+        font-weight: 100;
+        font-family: $lato;
+    }
+    a {
+        color: #fff;
+        padding: 5px 20px;
+        border-radius: 7px;
+        font-family: $lato;
+        display: inline-block;
+        text-decoration: none;
+        border: 2px solid #fff;
+        transition: all .3s ease;
+        &:hover {
+            background: rgba(255, 255, 255, 0.4);
+        }
+    }
+    .col-sm-8 {
+        padding: 20px;
+    }
+    .col-sm-4 {
+        a {
+            margin-top: 28px;
+        }
+    }
+</style>
