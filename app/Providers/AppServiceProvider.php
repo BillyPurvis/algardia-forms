3e69cd5dd85ef18c1b7c0f83230264c6adfcd3af
Billy Purvis <purvisbilly@outlook.com>
2017-07-01 14:00:12 +0100

    sets request types to HTTPS for production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+
+        Schema::defaultStringLength(191);
+
+        if(env('APP_ENV') == 'production') {
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
 
     /**
